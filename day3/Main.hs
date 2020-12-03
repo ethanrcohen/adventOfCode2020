88265f56703cb037beb0d58c69306e7f5fdab793
Ethan Cohen <ecohen@yext.com>
2020-12-03 10:42:59 -0500

    Day 3

---
+++ day3/Main.hs
@@ -0,0 +1,18 @@
+module Main where 
+
+accumulatorWithSlope :: Int -> (Int, Int) -> String -> (Int, Int)
+accumulatorWithSlope x (sum, lastInd) row = if ((cycle row) !! (lastInd + x)) == '#' then (sum+1, lastInd+x) else (sum, lastInd+x)
+
+findTreesWithSlope :: (Int, Int) -> String -> Int
+findTreesWithSlope (x, y) s = fst $ foldl (accumulatorWithSlope x) (0, -1*x) (filterToEveryNthEntry y (lines s))
+
+filterToEveryNthEntry :: Int -> [a] -> [a]
+filterToEveryNthEntry n xs = map snd (filter (\(i, _) -> i `mod` n == 0) (zip [0..] xs))
+
+
+main = do 
+        input <- readFile "input.txt" 
+        let part1 = findTreesWithSlope (3,1) input
+        print ( "part1 : " ++ show part1)
+        let part2 = part1 * (findTreesWithSlope (1,1) input) * (findTreesWithSlope (7,1) input) * (findTreesWithSlope (5,1) input) * (findTreesWithSlope (1,2) input) 
+        print ( "part 2: " ++ show part2)
