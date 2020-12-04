cc3633103d17ae3984e45654f84bb181b4d3a3a6
Ethan Cohen <ecohen@yext.com>
2020-12-03 20:47:23 -0500

    Day3: Clean up style a bit

--- day3/Main.hs
+++ day3/Main.hs
@@ -4,11 +4,10 @@ accumulatorWithSlope :: Int -> (Int, Int) -> String -> (Int, Int)
 accumulatorWithSlope x (sum, lastInd) row = if ((cycle row) !! (lastInd + x)) == '#' then (sum+1, lastInd+x) else (sum, lastInd+x)
 
 findTreesWithSlope :: (Int, Int) -> String -> Int
-findTreesWithSlope (x, y) s = fst $ foldl (accumulatorWithSlope x) (0, -1*x) (filterToEveryNthEntry y (lines s))
+findTreesWithSlope (x, y) s = fst . foldl (accumulatorWithSlope x) (0, -1*x) . filterToEveryNthEntry y $ lines s
 
 filterToEveryNthEntry :: Int -> [a] -> [a]
-filterToEveryNthEntry n xs = map snd (filter (\(i, _) -> i `mod` n == 0) (zip [0..] xs))
-
+filterToEveryNthEntry n xs = map snd $ filter (\(i, _) -> i `mod` n == 0) $ zip [0..] xs
 
 main = do 
         input <- readFile "input.txt" 
